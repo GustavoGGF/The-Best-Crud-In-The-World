0ac7ec390ad40456ae8009b27639c3ad3910b715
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-17 00:01:12 -0300

    feat(backend): getUserProfile
    Bootstrap getUserProfile endpoint and initial service structure

--- backend/src/main/java/backend/controller/UserProfileController.java
+++ backend/src/main/java/backend/controller/UserProfileController.java
@@ -1,14 +1,25 @@
 package backend.controller;
 
 import backend.model.UserProfileData;
+import backend.repository.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-public class UserProfile {
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserProfileController {
 
+    private final UserProfileRepository userProfileRepository;
+
+    @GetMapping("/get-all-users")
     public List<UserProfileData> getUserProfile() {
-        return null;
+        List<UserProfileData> profiles = userProfileRepository.getUserAllProfile();
+
+        return (profiles == null || profiles.isEmpty()) ? null : profiles;
     }
 }
