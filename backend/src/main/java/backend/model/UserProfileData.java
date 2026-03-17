6f95b2eee4f585b02799c4d3225f663a002aa692
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-17 12:35:20 -0300

    refactor(backend): UserProfileData
    Consolidate UserProfile model into a single unified entity

--- backend/src/main/java/backend/model/UserProfileData.java
+++ backend/src/main/java/backend/model/UserProfileData.java
@@ -1,12 +1,15 @@
 package backend.model;
 
+import java.util.List;
+
 public record UserProfileData(
         Integer id,
         String name,
-        String perfis
+        List<String> perfis
 ) {
-
     public String getName() {
         return this.name;
     }
+    public Integer getID() { return this.id;}
+    public List<String> getPerfis() { return this.perfis;}
 }
