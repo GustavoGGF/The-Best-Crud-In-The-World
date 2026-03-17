9e65391b0c29e7e9b20fb3a249b01de76a572331
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-16 23:59:17 -0300

    feat(backend): GlobalExceptionHandler
    Implement global exception handler for SQL query errors

---
+++ backend/src/main/java/backend/controller/UserProfileController.java
@@ -0,0 +1,14 @@
+package backend.controller;
+
+import backend.model.UserProfileData;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserProfile {
+
+    public List<UserProfileData> getUserProfile() {
+        return null;
+    }
+}
---
+++ backend/src/main/java/backend/services/SystemProfileServices.java
@@ -0,0 +1,11 @@
+package backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SystemProfileServices {
+
+    public void insertNewProfile() {
+
+    }
+}
