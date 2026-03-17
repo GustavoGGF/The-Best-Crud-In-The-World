36496ccaeee38571896e7a166cc83fd8f0bb26b7
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-17 12:37:26 -0300

    refactor(backend): SystemProfileService
    Implement retrieval and deletion logic for system profiles

--- backend/src/main/java/backend/services/SystemProfileServices.java
+++ backend/src/main/java/backend/services/SystemProfileServices.java
@@ -1,14 +1,29 @@
 package backend.services;
 
+import backend.model.SystemProfileData;
 import backend.repository.SystemProfileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SystemProfileServices {
 
     private final SystemProfileRepository systemProfileRepository;
+
+    public List<SystemProfileData> findAll(){
+        List<SystemProfileData> profiles = systemProfileRepository.getAllProfiles();
+
+        if (profiles == null || profiles.isEmpty()) {
+            profiles = Collections.emptyList();
+        }
+
+        return profiles;
+    }
 
     public void insertNewProfile(String descricao) {
         if (descricao.length() < 5) throw new IllegalArgumentException("Quantidade de caracteres insuficiente");
@@ -21,4 +36,9 @@ public class SystemProfileServices {
 
         systemProfileRepository.updateProfile(id, descricao);
     }
+
+    public void deleteProfile(Integer id){
+        systemProfileRepository.deleteProfile(id);
+    }
+
 }
