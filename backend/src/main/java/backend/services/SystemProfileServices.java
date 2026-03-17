73cf2f2dfe0a108a26d7a37532f87cda3824efbf
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-17 00:04:28 -0300

    feat(backend): SystemProfileService
    Implement service layer to handle system profile business logic

--- backend/src/main/java/backend/services/SystemProfileServices.java
+++ backend/src/main/java/backend/services/SystemProfileServices.java
@@ -1,11 +1,24 @@
 package backend.services;
 
+import backend.repository.SystemProfileRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SystemProfileServices {
 
-    public void insertNewProfile() {
+    private final SystemProfileRepository systemProfileRepository;
 
+    public void insertNewProfile(String descricao) {
+        if (descricao.length() < 5) throw new IllegalArgumentException("Quantidade de caracteres insuficiente");
+
+        systemProfileRepository.createNewProfile(descricao);
+    }
+
+    public void updateProfile(Integer id, String descricao) {
+        if (descricao.length() < 5) throw new IllegalArgumentException("Quantidade de caracteres insuficiente");
+
+        systemProfileRepository.updateProfile(id, descricao);
     }
 }
