7da8974e5c0aa15fb1fb31a61d628f67b5b653bb
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-17 00:03:46 -0300

    feat(backend): UserProfileRepository
    Implement initial repository interface for user profile data access

--- backend/src/main/java/backend/repository/UserProfileRepository.java
+++ backend/src/main/java/backend/repository/UserProfileRepository.java
@@ -1,4 +1,25 @@
 package backend.repository;
 
+import backend.model.UserProfileData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class UserProfileRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<UserProfileData> getUserAllProfile() {
+        String sql = "select * from usuarios";
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> new UserProfileData(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("perfis ")
+        ));
+    }
 }
