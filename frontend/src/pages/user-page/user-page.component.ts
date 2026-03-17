fa7b1b43c4695d59910af9e9dc01dd9fd34c6272
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-03-17 00:10:10 -0300

    refactor(frontend): UserPageComponent
    Scaffold initial CRUD structure for user management

--- frontend/src/pages/user-page/user-page.component.ts
+++ frontend/src/pages/user-page/user-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import axios from "axios";
+import { SystemProfile } from "src/interfaces/SystemProfile";
+import { UserProfile } from "src/interfaces/UserProfile";
 
 @Component({
   selector: "app-user-page",
@@ -8,13 +11,17 @@ import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class UserPageComponent implements OnInit {
   userForm: FormGroup;
+  isEditing: boolean = false;
 
   // Lista de perfis disponíveis no sistema para o usuário escolher
-  perfisDisponiveis = ["Admin", "Editor", "Visualizador", "Gerente", "Suporte"];
+  perfisDisponiveis: SystemProfile[] = [];
+  users: UserProfile[] = [];
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.getSystemProfileData();
+    this.getUserProfileData();
     this.userForm = this.fb.group({
       nome: ["", [Validators.required, Validators.minLength(10)]],
       perfis: this.fb.array([], [Validators.required, Validators.minLength(1)]),
@@ -26,24 +33,55 @@ export class UserPageComponent implements OnInit {
     return this.userForm.get("perfis") as FormArray;
   }
 
-  onCheckboxChange(e: any) {
-    const perfis: FormArray = this.perfisFormArray;
-    if (e.target.checked) {
-      perfis.push(this.fb.control(e.target.value));
-    } else {
-      const index = perfis.controls.findIndex(
-        (x) => x.value === e.target.value,
+  async getSystemProfileData() {
+    try {
+      const response = await axios.get<SystemProfile[]>(
+        "/profile/get-all-profiles",
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        },
       );
-      perfis.removeAt(index);
+      console.log("response: ", response);
+
+      this.perfisDisponiveis = (response.data || []).sort(
+        (a, b) => a.id - b.id,
+      );
+    } catch (err) {
+      console.error("Erro ao buscar os perfis: ", err);
+      throw err;
     }
   }
 
-  salvar() {
-    if (this.userForm.valid) {
-      console.log("Usuário Criado:", this.userForm.value);
-      alert("Usuário salvo com sucesso!");
-      this.userForm.reset();
-      // Limpar os checkboxes manualmente se necessário ou via ViewChild
+  async getUserProfileData() {
+    try {
+      const response = await axios.get<UserProfile[]>("/user/get-all-users", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("response: ", response);
+
+      this.users = (response.data || []).sort((a, b) => a.id - b.id);
+    } catch (err) {
+      console.error("Erro ao buscar os perfis: ", err);
+      throw err;
     }
   }
+
+  sendData() {}
+
+  deleteUser() {}
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.userForm.get(fieldName);
+    return !!(field && field.invalid && field.touched);
+  }
+
+  clearForm() {}
+
+  editUser() {}
 }
--- frontend/src/pages/user-page/user-page.module.ts
+++ frontend/src/pages/user-page/user-page.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from "@angular/common";
 import { UserPageComponent } from "./user-page.component";
 import { RouterModule, Routes } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
+import { ProfileCheckboxModule } from "src/components/profile-checkbox/profile-checkbox.module";
+import { GeneratorListModule } from "src/components/generator-list/generator-list.module";
 
 const routes: Routes = [{ path: "", component: UserPageComponent }];
 
 @NgModule({
   declarations: [UserPageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    ProfileCheckboxModule,
+    GeneratorListModule,
+  ],
 })
 export class UserPageModule {}
